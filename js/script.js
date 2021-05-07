ab35274e1baebfb9da7a5d8a1b66e6616f1e9268
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-05-07 21:39:42 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -13,18 +13,39 @@
     const render = () => {
         let htmlString = "";
 
-        for (const task of tasks){
-            htmlString +=`
-            <li ${task.done ? "class=\"form__item form__item--line\"" : "class=\"form__item \"" }>
+        for (const task of tasks) {
+            htmlString += `
+            <li ${task.done ? "class=\"form__item form__item--line\"" : "class=\"form__item \""}>
             ${task.content}
             </li>
             `
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") return;
+
+        addNewTask(newTaskContent);
     }
 
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
